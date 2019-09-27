62db61874b058832fd4ed0d86dfeda3e3d9ff89e
kmes055 <kmes055@naver.com>
2019-09-27 15:25:26 +0900

    refactor ionic

---
+++ liverGAN/src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCheckbox, IonList, IonItem, IonLabel, IonNote, IonBadge, IonFabButton, IonFab, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import React from 'react';
+import { attachEventProps } from '@ionic/react/dist/types/components/utils';
+import { RouteComponentProps } from 'react-router';
+
+const Home: React.FC<RouteComponentProps> = (props) => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Ionic Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonCheckbox slot="start" />
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run Idea by Brandy</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              5 Days
+            </IonBadge>
+          </IonItem>
+        </IonList>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => props.history.push('/new')}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
